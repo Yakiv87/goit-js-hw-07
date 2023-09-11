e9365526f474d28ac33b4edc9c96cf85b435d21d
Yakiv87 <130331039+Yakiv87@users.noreply.github.com>
2023-09-10 23:13:36 -0400

    up

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,30 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryList = document.querySelector('.gallery');
+const galleryItemMarkup = createGalleryMarkup(galleryItems);
+galleryList.insertAdjacentHTML("beforeend", galleryItemMarkup);
+
+function createGalleryMarkup(galleryItems) {
+  return galleryItems
+  .map(({
+    preview, original, description}) => { 
+      return `
+  <li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>
+</li>`;
+  }).join("");
+}
+
+let lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250,
+  });
